293326e1a3ddf8e6e3502c136c795567312f5b6b
Sunit Chatterjee <34455433+chatterjeesunit@users.noreply.github.com>
2018-06-11 13:04:39 +0530

    re-build war file

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -4,7 +4,7 @@
     angular
         .module('demoApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
-        .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1528645330651)
+        .constant('DEBUG_INFO_ENABLED', true)
+        .constant('BUILD_TIMESTAMP', "")
 ;
 })();
